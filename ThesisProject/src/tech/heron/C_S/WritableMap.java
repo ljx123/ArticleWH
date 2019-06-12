e845d42842aaa07421d9a96299ee28de21f8efdf
ljx123 <1032946025@qq.com>
2019-06-12 16:26:29 +0800

    错漏修改

--- ThesisProject/src/tech/heron/C_S/WritableMap.java
+++ ThesisProject/src/tech/heron/C_S/WritableMap.java
@@ -19,15 +19,16 @@ public class WritableMap extends HashMap<String, String> implements Writable {
 	public void write(DataOutput out) throws IOException {
 		// TODO Auto-generated method stub
 		String string = this.toString();
+		System.out.print("========" + string + "=========\n");
 		out.writeChars(string);
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
 		// TODO Auto-generated method stub
-		String readLine = in.readLine();
+		String readLine = in.readUTF();
 		String keyValues = readLine.substring(1, readLine.length()-1);
-		String[] split = keyValues.split(",");
+		String[] split = keyValues.split(", ");
 		for (int i = 0; i < split.length; i++) {
 			String[] split2 = split[i].split("=");
 			this.put(split2[0], split2[1]);
